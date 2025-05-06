f76b24400670a1f4e2e7704b0e5a285bc06ab666
PankoU963 <alejandro_franco82211@elpoli.edu.co>
2025-05-06 14:14:11 -0500

    Movimiento del Jugador y Cámara

---
+++ RPGTaining/Assets/Scripts/Relacionado al Jugador/Cámara/FSCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    private float horizontalAxis;
+    private float verticalAxis;
+
+    private new Transform camera;
+    public Vector2 sens = new Vector2(1f,1f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = transform.Find("Camera");
+        Cursor.lockState = CursorLockMode.Locked;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Mouse X");
+        verticalAxis = Input.GetAxis("Mouse Y");
+
+        if(horizontalAxis != 0){
+            transform.Rotate(Vector3.up * horizontalAxis * sens.x);
+        }
+        
+        if(verticalAxis != 0){
+            //camera.Rotate(Vector3.left * verticalAxis * sens.y);
+            float angle = (camera.localEulerAngles.x - verticalAxis * sens.y + 360) % 360;
+            if(angle > 180) {angle -=360;}
+            angle = Mathf.Clamp(angle, -90, 90);
+
+            camera.localEulerAngles = Vector3.right * angle;
+        }
+    }
+}
