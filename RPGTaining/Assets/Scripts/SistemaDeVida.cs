53d75e157c3dad1d6ab6b13d392cdec0b4a96eb9
TheFoolBrave <santiago_bravo82211@elpoli.edu.co>
2025-05-05 22:46:03 -0500

    ssz

--- RPGTaining/Assets/Scripts/SistemaDeVida.cs
+++ RPGTaining/Assets/Scripts/SistemaDeVida.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SistemaDeVida : SistemaDeEstadisticas
 {
+    [SerializeField] private Slider slider;
+
     // Atributos
     public SistemaDeVida(int valorMax, int valorMin, int valorActual) : base(valorMax, valorMin, valorActual)
     {
         valorMax = 100;
         valorMin = 0;
     }
+    
+    
 }
