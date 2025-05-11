09021e1a79ba6e11933cf5bb25e7d66a093ba163
PankoU963 <alejandro_franco82211@elpoli.edu.co>
2025-05-10 23:49:22 -0500

    correccion

---
+++ RPGTaining/Assets/Scripts/Habilidades/Habilidades Mago/CuracionDivina.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CuracionDivina : Habilidad
+{
+    public CuracionDivina(string nombre) : base(nombre, null, "Descripción Curación", 30, 12)
+    {
+    }
+
+    public override void Lanzar()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
